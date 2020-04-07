bd2129a03dfcf22ccc3d9787c95cd678e705a644
Delowar <delowardev@gmail.com>
2020-04-07 20:04:38 +0600

    Extra font removed

--- comps/Head.js
+++ comps/Head.js
@@ -5,7 +5,7 @@ function IndexPage() {
         <Head>
             <title>Color to CSS Filter Generator</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <link href="https://fonts.googleapis.com/css?family=Fira+Mono|Hind+Siliguri:400,600&display=swap" rel="stylesheet"></link>
+            <link href="https://fonts.googleapis.com/css?family=Fira+Mono&display=swap" rel="stylesheet"></link>
         </Head>
     )
 }
